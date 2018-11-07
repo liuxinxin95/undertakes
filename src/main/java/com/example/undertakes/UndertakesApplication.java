ac11e3b99d67bab99ae54b16222fb5d10a1650fd
liuxinxin <18634318474@163. com>
2018-11-07 15:34:20 +0800

    项目创建

---
+++ src/main/java/com/example/undertakes/UndertakesApplication.java
@@ -0,0 +1,12 @@
+package com.example.undertakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndertakesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UndertakesApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/undertakes/common/utils/ExcelUtil.java
@@ -0,0 +1,94 @@
+package com.example.undertakes.common.utils;
+
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+
+public class ExcelUtil {
+
+    /**
+     * 导出Excel
+     * @param sheetName sheet名称
+     * @param title 标题
+     * @param values 内容
+     * @param wb HSSFWorkbook对象
+     * @return
+     */
+    public static HSSFWorkbook getHSSFWorkbook(String sheetName,String []title,String [][]values, HSSFWorkbook wb){
+
+        // 第一步，创建一个HSSFWorkbook，对应一个Excel文件
+        if(wb == null){
+            wb = new HSSFWorkbook();
+        }
+
+        // 第二步，在workbook中添加一个sheet,对应Excel文件中的sheet
+        HSSFSheet sheet = wb.createSheet(sheetName);
+
+        // 第三步，在sheet中添加表头第0行,注意老版本poi对Excel的行数列数有限制
+        HSSFRow row = sheet.createRow(0);
+
+        // 第四步，创建单元格，并设置值表头 设置表头居中
+        HSSFCellStyle style = wb.createCellStyle();
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER); // 创建一个居中格式
+
+        //声明列对象
+        HSSFCell cell = null;
+
+        //创建标题
+        for(int i=0;i<title.length;i++){
+            cell = row.createCell(i);
+            cell.setCellValue(title[i]);
+            cell.setCellStyle(style);
+        }
+
+        //创建内容
+        for(int i=0;i<values.length;i++){
+            row = sheet.createRow(i + 1);
+            for(int j=0;j<values[i].length;j++){
+                //将内容按顺序赋给对应的列对象
+                row.createCell(j).setCellValue(values[i][j]);
+            }
+        }
+        return wb;
+    }
+
+    public static String getCellValue(Cell cell){
+        String value = null;
+        switch (cell.getCellTypeEnum()) {
+            case NUMERIC: // 数字
+                //如果为时间格式的内容
+                if (org.apache.poi.ss.usermodel.DateUtil.isCellDateFormatted(cell)) {
+                    //注：format格式 yyyy-MM-dd hh:mm:ss 中小时为12小时制，若要24小时制，则把小h变为H即可，yyyy-MM-dd HH:mm:ss
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                    value=sdf.format(HSSFDateUtil.getJavaDate(cell.
+                            getNumericCellValue())).toString();
+                    break;
+                } else {
+                    value = new DecimalFormat().format(cell.getNumericCellValue());
+                }
+                break;
+            case STRING: // 字符串
+                value = cell.getStringCellValue();
+                break;
+            case BOOLEAN: // Boolean
+                value = cell.getBooleanCellValue() + "";
+                break;
+            case FORMULA: // 公式
+                value = cell.getCellFormula() + "";
+                break;
+            case BLANK: // 空值
+                value = "";
+                break;
+            case ERROR: // 故障
+                value = "非法字符";
+                break;
+            default:
+                value = "未知类型";
+                break;
+        }
+        return value;
+
+    }
+}
