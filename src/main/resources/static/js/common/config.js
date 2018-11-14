ae71b3a9bf8850d65c27d4b0f049c93386180254
liuxinxin <18634318474@163. com>
2018-11-14 13:53:34 +0800

    h-ui前端

---
+++ src/main/resources/static/js/common/config.js
@@ -0,0 +1,11 @@
+// var user_domain="http://localhost:8083";
+// var user_domain="http://localhost:8081";
+var user_domain="http://47.98.45.188:8083";
+var shop_domain="http://47.97.207.41:8085";
+//开发环境
+// var api_domain = "http://47.97.207.41:8081/";
+//var api_domain = "http://192.168.11.11:8081/";
+//测试环境
+var api_domain = "https://ceshi-apigateway.cangrongt.com/";
+//本地环境
+// var api_domain = "http://localhost:8081/";
